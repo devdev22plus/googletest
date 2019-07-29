99288fe0186c516e58a7e7f8df9f14ac9f12b620
x-cyber@windowslive.com <x-cyber@windowslive.com>
2019-07-29 16:33:10 +0700

    go

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace googletest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Hello World Super dotnet");
+        }
+    }
+}
